3f9aebbeae7c9c713f299f9741762f15affe759f
Jesus Lozano <jalozano841@gmail.com>
2020-05-10 18:36:58 -0500

    finished web site

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -11,13 +11,13 @@ Vue.use(firestorePlugin);
 import * as firebaseui from "firebaseui";
 
 const firebaseConfig = {
-    apiKey: process.env.VUE_APP_apiKey,
-    authDomain: process.env.VUE_APP_authDomain,
-    databaseURL: process.env.VUE_APP_databaseURL,
-    projectId: process.env.VUE_APP_projectId,
-    storageBucket: process.env.VUE_APP_storageBucket,
-    messagingSenderId: process.env.VUE_APP_messagingSenderId,
-    appId: process.env.VUE_APP_appId
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  databaseURL: process.env.VUE_APP_databaseURL,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId
 };
 
 firebase.initializeApp(firebaseConfig);
